66d94b3e39885a6d53de643740be06586c660b70
Steven Ho <steven.ho01@outlook.com>
2025-02-25 13:04:25 -0500

    Transactions by category list

--- app/src/main/java/com/inf8405/expensetracker/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/inf8405/expensetracker/ui/screens/HomeScreen.kt
@@ -1,7 +1,16 @@
 package com.inf8405.expensetracker.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -19,11 +28,16 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.inf8405.expensetracker.database.entities.CategoryEntity
+import com.inf8405.expensetracker.database.entities.TransactionEntity
 import com.inf8405.expensetracker.models.MainViewModelsWrapper
 import com.inf8405.expensetracker.models.TransactionType
 import com.inf8405.expensetracker.ui.navigation.ExpenseTrackerScreen
+import com.inf8405.expensetracker.utils.toColor
+import com.inf8405.expensetracker.viewmodels.CategoryViewModel
 import com.inf8405.expensetracker.viewmodels.TransactionViewModel
 import java.util.Locale
 
@@ -33,24 +47,33 @@ fun HomeScreen(
     navController: NavController,
     modifier: Modifier = Modifier
 ) {
-    val transactionViewModel: TransactionViewModel = mainViewModelsWrapper.transactionViewModel;
-    val balance by transactionViewModel.balance.collectAsState();
+    val transactionViewModel: TransactionViewModel = mainViewModelsWrapper.transactionViewModel
+    val categoryViewModel: CategoryViewModel = mainViewModelsWrapper.categoryViewModel
+    val balance by transactionViewModel.balance.collectAsState()
+    val expenses by transactionViewModel.expenses.collectAsState()
+    val income by transactionViewModel.income.collectAsState()
+    val expenseCategories by categoryViewModel.expenseCategories.collectAsState()
+    val incomeCategories by categoryViewModel.incomeCategories.collectAsState()
 
     var selectedTransactionTabIndex by rememberSaveable { mutableIntStateOf(0) }
     var selectedPeriodTabIndex by rememberSaveable { mutableIntStateOf(1) }
 
+    // TODO Need period
+    val expensesByCategory = groupByCategory(expenses, expenseCategories)
+    val incomeByCategory = groupByCategory(income, incomeCategories)
+
     Column(
-        modifier = Modifier
+        modifier = modifier
             .padding(10.dp)
     ) {
         Text(
             text = String.format(Locale.CANADA_FRENCH, "Solde : %.2f $", balance),
-            modifier = Modifier.padding(5.dp)
+            modifier = modifier.padding(5.dp)
         )
 
         TabRow(
             selectedTabIndex = selectedTransactionTabIndex,
-            modifier = Modifier
+            modifier = modifier
                 .padding(horizontal = 20.dp)
         ) {
             listOf(
@@ -60,16 +83,14 @@ fun HomeScreen(
                 Tab(
                     selected = selectedTransactionTabIndex == index,
                     onClick = { selectedTransactionTabIndex = index }) {
-                    Text(text = financialTab, modifier = Modifier.padding(vertical = 10.dp))
+                    Text(text = financialTab, modifier = modifier.padding(vertical = 10.dp))
                 }
             }
         }
 
-        ElevatedCard(
-            modifier = Modifier
-                .padding(horizontal = 2.dp)
-                .padding(top = 10.dp)
-        ) {
+        Spacer(modifier = modifier.height(10.dp))
+
+        ElevatedCard {
             TabRow(
                 selectedTabIndex = selectedPeriodTabIndex
             ) {
@@ -82,7 +103,7 @@ fun HomeScreen(
                     Tab(
                         selected = selectedPeriodTabIndex == index,
                         onClick = { selectedPeriodTabIndex = index }) {
-                        Text(text = period, modifier = Modifier.padding(vertical = 10.dp))
+                        Text(text = period, modifier = modifier.padding(vertical = 10.dp))
                     }
                 }
 
@@ -96,7 +117,7 @@ fun HomeScreen(
                 onClick = {
                     navController.navigate(ExpenseTrackerScreen.NewTransaction.name)
                 },
-                modifier = Modifier
+                modifier = modifier
                     .padding(8.dp)
                     .align(Alignment.End),
                 shape = CircleShape
@@ -104,6 +125,59 @@ fun HomeScreen(
                 Icon(Icons.Default.Add, contentDescription = "Add")
             }
         }
+
+        LazyColumn(
+            modifier = modifier
+                .padding(vertical = 20.dp)
+                .fillMaxWidth()
+        ) {
+            val transactions =
+                if (selectedTransactionTabIndex == 0) expensesByCategory else incomeByCategory
+            transactions.forEach { transaction ->
+                item {
+                    ElevatedCard(
+                        modifier = modifier
+                            .fillMaxWidth()
+                    ) {
+                        Row(
+                            modifier = modifier.padding(8.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(10.dp)
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .size(30.dp)
+                                    .clip(CircleShape)
+                                    .background(transaction.third.toColor())
+                            )
+
+                            Text(
+                                text = String.format(
+                                    Locale.CANADA_FRENCH,
+                                    "%s : %.2f $",
+                                    transaction.first,
+                                    transaction.second
+                                )
+                            )
+                        }
+                    }
+                    Spacer(modifier = modifier.height(10.dp))
+                }
+            }
+        }
     }
 }
 
+fun groupByCategory(
+    transactions: List<TransactionEntity>,
+    categories: List<CategoryEntity>
+): List<Triple<String, Double, String>> {
+    return transactions
+        .groupBy { it.category }
+        .map { (categoryName, transactions) ->
+            val totalAmount = transactions.sumOf { it.amount }
+            val categoryColor = categories.find { it.name == categoryName }?.color ?: "#000000"
+
+            Triple(categoryName, totalAmount, categoryColor)
+        }
+}
